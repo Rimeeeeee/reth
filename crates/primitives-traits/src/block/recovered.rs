4087471f2c156046acdad69ec7952d5e58cef508
Ishika Choudhury <117741714+Rimeeeeee@users.noreply.github.com>
2026-01-01 17:46:53 +0530

    fixes: added bal hash

--- crates/primitives-traits/src/block/recovered.rs
+++ crates/primitives-traits/src/block/recovered.rs
@@ -449,6 +449,11 @@ impl<B: Block> BlockHeader for RecoveredBlock<B> {
         self.header().requests_hash()
     }
 
+    ///#[cfg(feature = "amsterdam")]
+    fn block_access_list_hash(&self) -> Option<B256> {
+        self.header().block_access_list_hash()
+    }
+
     fn extra_data(&self) -> &Bytes {
         self.header().extra_data()
     }
